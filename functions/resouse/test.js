7ae01a1962dbbc898614119d7e24cf2359d46e65
tiajinsha <754671297@qq.com>
2022-11-28 17:24:32 +0800

    awda

--- functions/resouse/test.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    return await handleRequest(request);
-  },
-};
-
-async function handleRequest(request) {
-  return new Response('Hello world');
-}
