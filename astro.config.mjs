b810e7b83d6324e676c063b532c008a588d3404b
tiajinsha <754671297@qq.com>
2022-11-28 12:36:20 +0800

    awd

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({ mode: 'directory' }),
+  adapter: cloudflare({ mode: '"advanced"' }),
   /*   adapter: node({
     mode: 'standalone',
   }), */
--- functions/api/getComments.ts
+++
@@ -1,9 +0,0 @@
-import type { APIContext } from 'astro';
-import { getComments } from 'src/utils/api';
-export const get = async ({ request }: APIContext) => {
-  const response = await getComments(request);
-  return new Response(JSON.stringify(response), {
-    status: response.status,
-  });
-};
-/* adawd */
