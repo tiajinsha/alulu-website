57b34c804cb461774e831037ab5a54e435280014
tiajinsha <754671297@qq.com>
2022-11-28 18:54:09 +0800

    ddd

--- functions/api/getVideoinfo.js
+++ functions/api/getVideoinfo.js
@@ -1,16 +1,8 @@
 export async function onRequestGet({ request }) {
   const url = new URL(request.url);
   const video_ids = url.searchParams.get('video_ids');
-  const response = await fetch(`http://165.227.181.143:8000/api/v1/video?video_ids=${video_ids}`, {
-    method: 'get',
-    mode: 'cors',
-    credentials: 'include',
-    headers: request.headers,
-  }).then((res) => res.json());
-  return new Response(
-    JSON.stringify({ data: response, status: response.status, statusText: response.statusText }),
-    {
-      status: response.status,
-    },
-  );
+
+  return new Response(JSON.stringify({ data: video_ids }), {
+    status: 200,
+  });
 }
