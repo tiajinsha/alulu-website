fbfa98a24617e9e2ef87ea12fd8ba6bc9306ef0b
tiajinsha <754671297@qq.com>
2022-11-28 10:10:53 +0800

    awd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,14 @@ import react from '@astrojs/react';
 import { fileURLToPath } from 'url';
 import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({ mode: 'advanced' }),
-  // adapter: cloudflare(),
+  adapter: cloudflare({ mode: 'directory' }),
   /*   adapter: node({
     mode: 'standalone',
   }), */
   integrations: [react()],
   vite: {
-    define: {
-      'process.env.ALULU_BASE_URL': JSON.stringify(process.env.ALULU_BASE_URL),
-      'process.env.REF_TOKEN_URL': JSON.stringify(process.env.REF_TOKEN_URL),
-      'process.env.ALULU_REGISTER_URL': JSON.stringify(process.env.ALULU_REGISTER_URL),
-      'process.env.TOKEN_NAME': JSON.stringify(process.env.TOKEN_NAME),
-    },
     /*     ssr: {
       noExternal: ['path-to-regexp'],
     }, */
