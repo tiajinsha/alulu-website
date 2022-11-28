a6d0e4d33342004c7b9604bf5afe84a162120162
tiajinsha <754671297@qq.com>
2022-11-28 14:50:12 +0800

    awda

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
-//import node from '@astrojs/node';
 import { fileURLToPath } from 'url';
 import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({ mode: 'advanced' }),
-  /*   adapter: node({
-    mode: 'standalone',
-  }), */
+  adapter: cloudflare(),
+
   integrations: [react()],
   vite: {
-    /*     ssr: {
+    ssr: {
       noExternal: ['path-to-regexp'],
-    }, */
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   },
 });
